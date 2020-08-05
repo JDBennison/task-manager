011f4403851408e2bc84d35f4cab4fc1dcbad336
JDBennison <jamesbennison88@gmail.com>
2020-08-05 15:17:11 +0000

    session user profile

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def register():
         # put the new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -56,7 +57,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username").capitalize()))
+                    flash("Welcome, {}".format(
+                        request.form.get("username").capitalize()))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 #invalid password match
                 flash("Incorrect Username and/or Password")
@@ -68,6 +72,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    #grab the session users username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 @app.route('/add_task')
 def add_task():
